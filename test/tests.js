afe727b6d82fd6b6e0c6215656de655d9f58488a
Tetsuya Mori <t2y3141592@gmail.com>
2016-04-29 12:44:36 +0900

    test: Remove non cleanJSON tests in sync with Fix #14

--- test/tests.js
+++ test/tests.js
@@ -10,39 +10,20 @@ function normalizeWhitespace(str) {
             .replace(/\n\s+/g, '\n');
 }
 
-var x1 = new xliff2json();
 var x2 = new xliff2json({
     cleanJSON:true,
     decorateJSON:true
   });
 
 var input = fs.readFileSync("test/fixture/test_less.xml",'utf8');
-var inOdd= fs.readFileSync("test/fixture/oddities.xml",'utf8');
-var outOdd= fs.readFileSync("test/fixture/oddities_out.xml",'utf8');
 
 describe('Round trip', function(){
-    it('just to xliff', function(done){
-      x1.parseXliff(input,function(xliffJson){
-        x1.parseJSON(xliffJson,function(output){
-          assert.equal(normalizeWhitespace(output),normalizeWhitespace(input));
-          done();
-        });
-      });
-    });
     it('to json', function(done){
       x2.parseXliff(input,function(json){
          x2.parseJSON(json,function(output){
            assert.equal(normalizeWhitespace(output),normalizeWhitespace(input));
            done();
          });
-      });
-    });
-    it('oddities to xliff', function(done){
-      x1.parseXliff(inOdd,function(xliffJson){
-        x1.parseJSON(xliffJson,function(output){
-          assert.equal(normalizeWhitespace(output),normalizeWhitespace(outOdd));
-          done();
-        });
       });
     });
 });
